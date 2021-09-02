3fbe65e8fa5a3157b84573741d81e22788b03052
allen-lhj <liu772636821@163.com>
2021-09-03 07:50:42 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+   <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      title: '生活一线通'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  .wrapper {
+    text-align: center;
+    color: red;
+  }
+</style>
